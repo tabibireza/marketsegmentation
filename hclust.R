d626136628d29ce805f54cef3ea2783664c00c66
Your Name <you@example.com>
2019-09-15 11:27:35 +0800

    hclust change

--- hclust.R
+++ hclust.R
@@ -9,16 +9,27 @@ str(seg_data)
 std_seg_data <- scale(seg_data[,c("Trendy", "Styling", "Reliability", "Sportiness", "Performance", "Comfort")])
 dist <- dist(std_seg_data, method = "euclidean")
 as.matrix(dist)[1:5,1:5]
+library(factoextra)
+fviz_dist(dist, gradient = list(low="#00AFBB", mid = "white", high = "#FC4E07"))
+
 
 # hierarchical clustering
 set.seed(1990)
 clust <- hclust(dist, method = "ward.D2")
 plot(clust)
 
-# cutree 4
+
+# Number of Clusters, we must use only 5 variables out of 6 to avoid collinearity issues.
+library(NbClust)
+set.seed(1990)
+NbClust(data=std_seg_data[,1:5], min.nc=3, max.nc=15, index="all", method="ward.D2")
+
+
+#cutree 4
 h_cluster <- cutree(clust, 4)
 rect.hclust(clust, k=4, border="red")
 table(h_cluster)
+
 
 #the clustering variables means by cluster
 hclust_summary <- aggregate(std_seg_data[,c("Trendy", "Styling", "Reliability", "Sportiness",
@@ -31,10 +42,15 @@ h_cluster <- cutree(clust, 3)
 rect.hclust(clust, k=3, border="red")
 table(h_cluster)
 
-#the clustering variables means by cluster
+#the clustering variables means by cluster- which is not like kmeans!
 hclust_summary <- aggregate(std_seg_data[,c("Trendy", "Styling", "Reliability", "Sportiness",
                                             "Performance", "Comfort")],by=list(h_cluster),FUN=mean)
 hclust_summary
+
+# allocating membors to cluster
+seg_data$cluster <- h_cluster
+
+
 
 #  rename those clusters according to their characteristics
 h_cluster <- factor(h_cluster,levels = c(1,2,3),labels = c("Perf.", "Comfort", "Appearance"))
@@ -42,10 +58,6 @@ h_cluster <- factor(h_cluster,levels = c(1,2,3),labels = c("Perf.", "Comfort", "
 #  focus on a given cluster
 plot(cut(as.dendrogram(clust), h=9)$lower[[3]])
 
-# Number of Clusters, we must use only 5 variables out of 6 to avoid collinearity issues.
-library(NbClust)
-set.seed(1990)
-NbClust(data=std_seg_data[,1:5], min.nc=3, max.nc=15, index="all", method="ward.D2")
 
 # Targeting the Clusters/segments  Demographics
 install.packages("gmodels")
